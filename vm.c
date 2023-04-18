b6caca355f28bae2ac7d03d7cd5fae91efcf0584
Ivan Kurokhtin <jumping.ig@gmail.com>
2023-04-18 18:58:47 +0300

    Chapter 22. Local variables

--- vm.c
+++ vm.c
@@ -120,7 +120,6 @@ static InterpretResult run() {
         }
         break;
       }
-
       case OP_GET_GLOBAL: {
         ObjString* name = READ_STRING();
         Value value;
@@ -129,6 +128,16 @@ static InterpretResult run() {
           return INTERPRET_RUNTIME_ERROR;
         }
         push(value);
+        break;
+      }
+      case OP_GET_LOCAL: {
+        uint8_t slot = READ_BYTE();
+        push(vm.stack[slot]);
+        break;
+      }
+      case OP_SET_LOCAL: {
+        uint8_t slot = READ_BYTE();
+        vm.stack[slot] = peek(0);
         break;
       }
 
